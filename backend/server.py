86073d445733befa7f16ff0a2a6c36d933bc91a4
Aprajita Kain <>
2024-01-31 16:19:12 +0100

    Enable CORS and Shell script to start client server

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
 
 @app.route("/members")
 def members():
-    return {"members": ["Member1","Member2","Member3"]}
+    return {"members": ["Member1", "Member2", "Member3"]}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ start_instagram.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Function to find and kill processes running on a specific port
+kill_process_on_port() {
+    local port=$1
+    local pid=$(lsof -t -i:$port)
+    if [ -n "$pid" ]; then
+        echo "Killing process on port $port (PID: $pid)"
+        kill -9 $pid
+    fi
+}
+
+# Function to handle cleanup
+cleanup() {
+    # Kill the Flask server process
+    pkill -f "python3 server.py"
+
+    # Kill the React server process on port 3000
+    kill_process_on_port 3000
+     # Kill the React server process on port 5000
+    kill_process_on_port 5000
+
+}
+
+# Trap the EXIT signal to call the cleanup function
+trap cleanup EXIT
+
+# Kill any process running on port 5000
+kill_process_on_port 5000
+# Navigate to the backend folder
+cd backend
+
+# Start the backend server
+python3 server.py &
+
+# Wait for 3 seconds (adjust as needed)
+sleep 3
+
+# Navigate to the frontend folder
+cd ../
+cd frontend
+
+# Install dependencies if not already installed
+# You can skip this step if you've already installed the dependencies
+# npm install
+
+# Kill any process running on port 3000
+kill_process_on_port 3000
+
+# Start the frontend server
+npm start
